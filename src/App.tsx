904b1abd6b9fe66f8a807fdd93b8898c706b3a76
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-02-20 18:58:02 +0300

    Add text and search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Center,
+  Container,
+  Input,
+  MantineProvider,
+  Paper,
+  Text,
+} from "@mantine/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Container py={200}>
+        <Paper sx={{ padding: "8px" }} shadow="xl">
+          <Center>
+            <Text>Search Your Pokemoon</Text>
+          </Center>
+          <Input
+            placeholder="Type here.."
+            rightSection={<Button>Search</Button>}
+            rightSectionWidth={-1}
+          />
+        </Paper>
+      </Container>
+    </MantineProvider>
   );
 }
 
