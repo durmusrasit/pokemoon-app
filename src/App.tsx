aeebaffd90b69b1adc4affb4507aedd265cde270
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2023-02-20 19:55:16 +0300

    Add margin between components

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,10 @@ function App() {
       <Container py={200}>
         <Paper sx={{ padding: "8px" }} shadow="xl">
           <Center>
-            <Title>Search Your Pokemoon</Title>
+            <Title style={{ marginBottom: 7 }}>Search Your Pokemoon</Title>
           </Center>
           <Input
+            style={{ marginBottom: 7 }}
             placeholder="Type here.."
             rightSection={<Button>Search</Button>}
             rightSectionWidth={-1}
